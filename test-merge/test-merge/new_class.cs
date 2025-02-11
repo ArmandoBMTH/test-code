e446af40e78049b2870903e0e830ff3f3b66d95a
Jesús Armando Sánchez Godínez <armandoc0re86@gmail.com>
2025-02-11 14:18:21 -0600

    error code

---
+++ test-merge/test-merge/new_class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_merge
+{
+    public class new_class
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
